a6b5a01e01cbf216276691202c82472c6f5d8d26
Karan Bhatia <karanbhatia2050@gmail.com>
2020-04-22 19:43:55 +0530

    data types supported

---
+++ Very-Basic/4.dart
@@ -0,0 +1,16 @@
+// https://dart.dev/guides/language/language-tour#built-in-types
+main(){
+  int a = 5;
+  double b = 5.646;
+  String c = "This is a string";
+  bool d = false;
+  var list = [1,2,3];
+  var set = {1,2,3};
+  var map = {
+    1:"one",
+    2:"Two",
+    3:"Three"
+  };
+  var rune = '👍';
+  print(" $a \n $b \n $c \n $d \n $list \n $set \n $map \n $rune");
+}
